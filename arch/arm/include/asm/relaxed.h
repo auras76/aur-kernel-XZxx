3d3dcf0d2319cb6d910f0e434821b0c3fc9aca15
auras76 <anghel0876@gmail.com>
2016-02-29 21:01:44 +0100

    UPDATE :NVIDIA power efficiency patches

--- arch/arm/include/asm/relaxed.h
+++ arch/arm/include/asm/relaxed.h
@@ -1,5 +1,5 @@
 /*
- * arm64/include/asm/relaxed.h
+ * arm/include/asm/relaxed.h
  *
  * Copyright (c) 2014 NVIDIA Corporation. All rights reserved.
  * This software is licensed under the terms of the GNU General Public
@@ -15,19 +15,6 @@
 #ifndef _ASM_RELAXED_H_
 #define _ASM_RELAXED_H_
 
-#include <asm/atomic.h>
-
-#ifndef cpu_relaxed_read_short
-#define cpu_relaxed_read_short(p)	(*(p))
-#endif
-
-#ifndef cpu_relaxed_read
-#define cpu_relaxed_read(p)		(*(p))
-#endif
-
-#ifndef cpu_relaxed_read_long
-#define cpu_relaxed_read_long(p)	(*(p))
-#endif
-
 #include <asm-generic/relaxed.h>
+
 #endif /*_ASM_RELAXED_H_*/
